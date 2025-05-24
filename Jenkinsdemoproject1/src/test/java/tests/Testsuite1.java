07e2b27ba4c2d8e12f1401453ce52799e52f1462
Sravani Velpucharla <sravanivelpucharla448@gmail.com>
2025-05-24 17:29:02 +0530

    sample project updated for jenkins run

---
+++ Jenkinsdemoproject1/src/test/java/tests/Testsuite1.java
@@ -0,0 +1,22 @@
+package tests;
+
+import org.junit.Test;
+
+public class Testsuite1 {
+
+	@Test
+	public void test1() {
+		System.out.println("test1");
+	}
+
+	@Test
+	public void test2() {
+		System.out.println("test2");
+	}
+
+	@Test
+	public void test3() {
+		System.out.println("test3");
+	}
+
+}
---
+++ Jenkinsdemoproject1/src/test/java/tests/Testsuite3.java
@@ -0,0 +1,24 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Testsuite3 {
+
+	@Test
+	public void test1() {
+		System.out.println("test1");
+	}
+
+	@Test
+	public void test2() {
+		System.out.println("test2");
+	}
+
+	@Test
+	public void test3() {
+		System.out.println("test3");
+	}
+
+}
